c0fa7f2e5223f56af14c7624fa3da69bbc201ef0
Riley Boyd <riley@donkeymob.com>
2022-04-18 15:44:45 -0400

    Example works, but trying to do something to illustrate what's going on. Work in progress.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import { useTransition } from 'react';
+import { useState, useTransition } from 'react';
 import './App.css';
 
 function App() {
+  const [hasBeenClicked, setHasBeenClicked] = useState(false);
+
+  const [isPending, startTransition] = useTransition();
+  const [count, setCount]= useState(0)
+
+  /*
+  //hmmm
+  const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+  const getCountUpdate = async (prevState) => {
+    await sleep(2000);
+    return prevState+1;
+  }
+  */
+
+
+  const clickHandler = (e) => {
+    
+    setCount(prevState=>{
+      setCount(prevState+1);
+    });
+
+    startTransition(()=>{
+      setHasBeenClicked(true);
+    })
+  }
+
+
   return (
     <div className="App">
       <section>
         <h1>React useTransition hook example</h1>
+        {isPending ? <p>Loading...</p> : <p>Button {hasBeenClicked ? 'has' : 'has not'} been clicked</p>}
+        <p>Button has been clicked {count} times.</p>
+        <button onClick={clickHandler} disabled={isPending}>Click Me</button>
       </section>
     </div>
   );
