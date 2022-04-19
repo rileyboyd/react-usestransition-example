8002856e907cc17788297a34c459ae9282135a31
Riley Boyd <contact@rileyboyd.com>
2022-04-18 23:56:30 -0400

    Adding Suspense

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
-import { useState, useTransition } from 'react';
-import './App.css';
+import { Suspense, useState, useTransition } from "react";
+import "./App.css";
 
 function App() {
   const [hasBeenClicked, setHasBeenClicked] = useState(false);
 
   const [isPending, startTransition] = useTransition();
-  const [count, setCount]= useState(0)
+  const [count, setCount] = useState(0);
 
-  /*
-  //hmmm
-  const sleep = ms => new Promise(r => setTimeout(r, ms));
+  const getNewCount = async () => {
+    return "abc";
+  };
 
-  const getCountUpdate = async (prevState) => {
-    await sleep(2000);
-    return prevState+1;
-  }
-  */
-
-
-  const clickHandler = (e) => {
-    
-    setCount(prevState=>{
-      setCount(prevState+1);
-    });
-
-    startTransition(()=>{
-      setHasBeenClicked(true);
-    })
-  }
-
+  const clickHandler = async (e) => {
+    // startTransition(async () => {
+    await new Promise((r) => setTimeout(r, 2000));
+    setHasBeenClicked(true);
+    // });
+  };
 
   return (
     <div className="App">
       <section>
         <h1>React useTransition hook example</h1>
-        {isPending ? <p>Loading...</p> : <p>Button {hasBeenClicked ? 'has' : 'has not'} been clicked</p>}
+
+        <Suspense fallback={<p>Loading...</p>}>
+          <p>Button {hasBeenClicked ? "has" : "has not"} been clicked</p>
+        </Suspense>
         <p>Button has been clicked {count} times.</p>
-        <button onClick={clickHandler} disabled={isPending}>Click Me</button>
+        <button onClick={clickHandler} disabled={isPending}>
+          Click Me
+        </button>
       </section>
     </div>
   );
